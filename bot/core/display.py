9be9f3b23671acc0d8dfe2065b2d04f1bb0b5e42
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-06 10:58:26 +0300

    Update display.py

--- bot/core/display.py
+++ bot/core/display.py
@@ -9,11 +9,11 @@ from pyrogram.types import Message, CallbackQuery
 from pyrogram.errors import FloodWait
 
 PROGRESS = """
-⏳ **Percentage:** `{0}%`
-✅ **Done:** `{1}`
-💠 **Total:** `{2}`
-📶 **Speed:** `{3}/s`
-🕰 **ETA:** `{4}`
+⏳ **Yüzde:** `{0}%`
+✅ **Tamamlanan:** `{1}`
+💠 **Toplam:** `{2}`
+📶 **Hız:** `{3}/s`
+🕰 **Süre:** `{4}`
 """
 
 
