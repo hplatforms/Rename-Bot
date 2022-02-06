e22bb42741893f8ab9fd2204af28462366485a2d
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-06 10:16:58 +0300

    Update settings.py

--- bot/core/handlers/settings.py
+++ bot/core/handlers/settings.py
@@ -10,36 +10,36 @@ async def show_settings(m: "types.Message"):
     usr_id = m.chat.id
     user_data = await db.get_user_data(usr_id)
     if not user_data:
-        await m.edit("Failed to fetch your data from database!")
+        await m.edit("Verileriniz Veritabanından Alınamadı!")
         return
     upload_as_doc = user_data.get("upload_as_doc", False)
     caption = user_data.get("caption", None)
     apply_caption = user_data.get("apply_caption", True)
     thumbnail = user_data.get("thumbnail", None)
     buttons_markup = [
-        [types.InlineKeyboardButton(f"Upload as Doc {'✅' if upload_as_doc else '❌'}",
+        [types.InlineKeyboardButton(f"Dosya Olarak Yükle {'✅' if upload_as_doc else '❌'}",
                                     callback_data="triggerUploadMode")],
-        [types.InlineKeyboardButton(f"Apply Caption {'✅' if apply_caption else '❌'}",
+        [types.InlineKeyboardButton(f"Altyazı Uygula {'✅' if apply_caption else '❌'}",
                                     callback_data="triggerApplyCaption")],
-        [types.InlineKeyboardButton(f"Apply Default Caption {'❌' if caption else '✅'}",
+        [types.InlineKeyboardButton(f"Varsayılan Altyazı Uygula {'❌' if caption else '✅'}",
                                     callback_data="triggerApplyDefaultCaption")],
-        [types.InlineKeyboardButton("Set Custom Caption",
+        [types.InlineKeyboardButton("Özel Altyazı Ayarla",
                                     callback_data="setCustomCaption")],
         [types.InlineKeyboardButton(f"{'Change' if thumbnail else 'Set'} Thumbnail",
                                     callback_data="setThumbnail")]
     ]
     if thumbnail:
-        buttons_markup.append([types.InlineKeyboardButton("Show Thumbnail",
+        buttons_markup.append([types.InlineKeyboardButton("Kayıtlı Thumbnail Göster",
                                                           callback_data="showThumbnail")])
     if caption:
-        buttons_markup.append([types.InlineKeyboardButton("Show Caption",
+        buttons_markup.append([types.InlineKeyboardButton("Kayıtlı Altyazı Göster",
                                                           callback_data="showCaption")])
     buttons_markup.append([types.InlineKeyboardButton("Close Message",
                                                       callback_data="closeMessage")])
 
     try:
         await m.edit(
-            text="**Here you can setup your settings:**",
+            text="**Buradan Ayarlarınızı Yapabilirsiniz:**",
             reply_markup=types.InlineKeyboardMarkup(buttons_markup),
             disable_web_page_preview=True,
             parse_mode="Markdown"
