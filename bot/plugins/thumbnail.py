8d18e14eba56352dcd9c4148a3f3704b2a528bb4
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-06 11:42:52 +0300

    Update thumbnail.py

--- bot/plugins/thumbnail.py
+++ bot/plugins/thumbnail.py
@@ -10,14 +10,14 @@ from bot.core.db.add import add_user_to_database
 @Client.on_message(filters.command("show_thumbnail") & filters.private & ~filters.edited)
 async def show_thumbnail(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Senin Hakkında Bilgim Yok! :(")
     await add_user_to_database(c, m)
     thumbnail = await db.get_thumbnail(m.from_user.id)
     if not thumbnail:
-        return await m.reply_text("You didn't set custom thumbnail!")
+        return await m.reply_text("Thumbnail Ayarlamadınız!")
     await c.send_photo(m.chat.id, thumbnail, caption="Custom Thumbnail",
                        reply_markup=types.InlineKeyboardMarkup(
-                           [[types.InlineKeyboardButton("Delete Thumbnail",
+                           [[types.InlineKeyboardButton("Thumbnail Sil",
                                                         callback_data="deleteThumbnail")]]
                        ))
 
@@ -25,15 +25,15 @@ async def show_thumbnail(c: Client, m: "types.Message"):
 @Client.on_message(filters.command("set_thumbnail") & filters.private & ~filters.edited)
 async def set_thumbnail(c: Client, m: "types.Message"):
     if (not m.reply_to_message) or (not m.reply_to_message.photo):
-        return await m.reply_text("Reply to any image to save in as custom thumbnail!")
+        return await m.reply_text("Thumbnail Ayarlamak İçin Resim Gönder ve /set_thumbnail Komutuyla Yanıtla!")
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Senin Hakkında Bilgim Yok! :(")
     await add_user_to_database(c, m)
     await db.set_thumbnail(m.from_user.id, m.reply_to_message.photo.file_id)
-    await m.reply_text("Okay,\n"
-                       "I will use this image as custom thumbnail.",
+    await m.reply_text("Tamam,\n"
+                       "Bu Resim Thumbnail Olarak Ayarlandı.",
                        reply_markup=types.InlineKeyboardMarkup(
-                           [[types.InlineKeyboardButton("Delete Thumbnail",
+                           [[types.InlineKeyboardButton("Thumbnail Sil",
                                                         callback_data="deleteThumbnail")]]
                        ))
 
@@ -41,8 +41,8 @@ async def set_thumbnail(c: Client, m: "types.Message"):
 @Client.on_message(filters.command("delete_thumbnail") & filters.private & ~filters.edited)
 async def delete_thumbnail(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Senin Hakkında Bilgim Yok! :(")
     await add_user_to_database(c, m)
     await db.set_thumbnail(m.from_user.id, None)
-    await m.reply_text("Okay,\n"
-                       "I deleted custom thumbnail from my database.")
+    await m.reply_text("Tamam,\n"
+                       "Thumbnail Veritabanından Silindi.")
