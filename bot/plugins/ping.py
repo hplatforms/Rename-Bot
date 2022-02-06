1e4fa023d444611a9fd6dcfd7ec85a8b9708e787
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-06 11:28:51 +0300

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -9,16 +9,16 @@ from bot.core.db.add import add_user_to_database
 @Client.on_message(filters.command(["start", "ping"]) & filters.private & ~filters.edited)
 async def ping_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Senin Hakkında Bilgim Yok! :(")
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
-             "I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.",
+        text="Merhaba, Ben {bot_name}!\n\n"
+             "Medyayı İndirmeden Yeniden Adlandırabilirim!\n"
+             "Hız, Medya DC'nize Bağlıdır.\n\n"
+             "Bana Medyayı Gönder ve /rename Komutuyla Cevapla.",
         reply_markup=types.InlineKeyboardMarkup([[
-           types.InlineKeyboardButton("Show Settings",
+           types.InlineKeyboardButton("Ayarları Göster",
                                       callback_data="showSettings")
         ]])
     )
@@ -27,16 +27,16 @@ async def ping_handler(c: Client, m: "types.Message"):
 @Client.on_message(filters.command("help") & filters.private & ~filters.edited)
 async def help_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Senin Hakkında Bilgim Yok! :(")
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.\n\n"
-             "To set custom thumbnail reply to any image with /set_thumbnail\n\n"
-             "To see custom thumbnail press /show_thumbnail",
+        text="Medyayı İndirmeden Yeniden Adlandırabilirim!\n"
+             "Hız, Medya DC'nize Bağlıdır.\n\n"
+             "Bana Medyayı Gönder ve /rename Komutuyla Cevapla.\n\n"
+             "Thumbnail Ayarlamak İçin Resim Gönder ve /set_thumnail Komutuyla Yanıtla\n\n"
+             "Ayarlı Thumbnail Görmek İçin /show_thumbnail Komutu Gönder.",
         reply_markup=types.InlineKeyboardMarkup([[
-           types.InlineKeyboardButton("Show Settings",
+           types.InlineKeyboardButton("Ayarları Göster",
                                       callback_data="showSettings")]])
     )
