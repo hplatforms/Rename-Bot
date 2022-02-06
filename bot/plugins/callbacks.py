d65a1271b4b3aa648c53868758d2d833bc5b29fc
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-06 11:16:18 +0300

    Update callbacks.py

--- bot/plugins/callbacks.py
+++ bot/plugins/callbacks.py
@@ -21,50 +21,50 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
     elif cb.data == "showThumbnail":
         thumbnail = await db.get_thumbnail(cb.from_user.id)
         if not thumbnail:
-            await cb.answer("You didn't set any custom thumbnail!", show_alert=True)
+            await cb.answer("Herhangi Bir Thumbnail Ayarlamadınız!", show_alert=True)
         else:
             await cb.answer()
             await c.send_photo(cb.message.chat.id, thumbnail, "Custom Thumbnail",
                                reply_markup=types.InlineKeyboardMarkup([[
-                                   types.InlineKeyboardButton("Delete Thumbnail",
+                                   types.InlineKeyboardButton("Thumbnail Sil",
                                                               callback_data="deleteThumbnail")
                                ]]))
     elif cb.data == "deleteThumbnail":
         await db.set_thumbnail(cb.from_user.id, None)
-        await cb.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
+        await cb.answer("Tamam, Thumbnail Silindi. Şimdi Varsayılan Thumbnail Uygulanacak.", show_alert=True)
         await cb.message.delete(True)
     elif cb.data == "setThumbnail":
         await cb.answer()
-        await cb.message.edit("Send me any photo to set that as custom thumbnail.\n\n"
-                              "Press /cancel to cancel process.")
+        await cb.message.edit("Thumbnail Ayarlamak İçin Bana Resim Gönder.\n\n"
+                              "İşlemi İptal Etmel İçin /cancel Komutu Verin.")
         from_user_thumb: "types.Message" = await c.listen(cb.message.chat.id)
         if not from_user_thumb.photo:
-            await cb.message.edit("Process Cancelled!")
+            await cb.message.edit("İşlem İptal Edildi!")
             return await from_user_thumb.continue_propagation()
         else:
             await db.set_thumbnail(cb.from_user.id, from_user_thumb.photo.file_id)
-            await cb.message.edit("Okay!\n"
-                                  "Now I will apply this thumbnail to next uploads.",
+            await cb.message.edit("Tamam!\n"
+                                  "Bu Thumbnail Sonraki İşlemlerde de Kullanılacak.",
                                   reply_markup=types.InlineKeyboardMarkup(
-                                      [[types.InlineKeyboardButton("Show Settings",
+                                      [[types.InlineKeyboardButton("Ayarları Göster",
                                                                    callback_data="showSettings")]]
                                   ))
     elif cb.data == "setCustomCaption":
         await cb.answer()
-        await cb.message.edit("Okay,\n"
-                              "Send me your custom caption.\n\n"
-                              "Press /cancel to cancel process.")
+        await cb.message.edit("Tamam,\n"
+                              "Bana Özel Başlık Gönder.\n\n"
+                              "İşlemi İptal Etmel İçin /cancel Komutu Verin.")
         user_input_msg: "types.Message" = await c.listen(cb.message.chat.id)
         if not user_input_msg.text:
-            await cb.message.edit("Process Cancelled!")
+            await cb.message.edit("İşlem İptal Edildi!")
             return await user_input_msg.continue_propagation()
         if user_input_msg.text and user_input_msg.text.startswith("/"):
-            await cb.message.edit("Process Cancelled!")
+            await cb.message.edit("İşlem İptal Edildi!")
             return await user_input_msg.continue_propagation()
         await db.set_caption(cb.from_user.id, user_input_msg.text.markdown)
-        await cb.message.edit("Custom Caption Added Successfully!",
+        await cb.message.edit("Özel Altyazı Başarıyla Eklendi!",
                               reply_markup=types.InlineKeyboardMarkup(
-                                  [[types.InlineKeyboardButton("Show Settings",
+                                  [[types.InlineKeyboardButton("Ayarları Göster",
                                                                callback_data="showSettings")]]
                               ))
     elif cb.data == "triggerApplyCaption":
@@ -77,19 +77,19 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
         await show_settings(cb.message)
     elif cb.data == "triggerApplyDefaultCaption":
         await db.set_caption(cb.from_user.id, None)
-        await cb.answer("Okay, now I will keep default caption.", show_alert=True)
+        await cb.answer("Tamam, Şimdi Varsayılan Başlığı Tutacağım.", show_alert=True)
         await show_settings(cb.message)
     elif cb.data == "showCaption":
         caption = await db.get_caption(cb.from_user.id)
         if not caption:
-            await cb.answer("You didn't set any custom caption!", show_alert=True)
+            await cb.answer("Herhangi Bir Özel Başlık Belirlemediniz!", show_alert=True)
         else:
             await cb.answer()
             await cb.message.edit(
                 text=caption,
                 parse_mode="Markdown",
                 reply_markup=types.InlineKeyboardMarkup([[
-                    types.InlineKeyboardButton("Go Back", callback_data="showSettings")
+                    types.InlineKeyboardButton("Geri", callback_data="showSettings")
                 ]])
             )
     elif cb.data == "triggerUploadMode":
@@ -103,11 +103,11 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
     elif cb.data == "showFileInfo":
         replied_m = cb.message.reply_to_message
         _file_name = get_media_file_name(replied_m)
-        text = f"**File Name:** `{_file_name}`\n\n" \
-               f"**File Extension:** `{_file_name.rsplit('.', 1)[-1].upper()}`\n\n" \
-               f"**File Type:** `{get_file_type(replied_m).upper()}`\n\n" \
-               f"**File Size:** `{humanbytes(get_media_file_size(replied_m))}`\n\n" \
-               f"**File MimeType:** `{get_file_attr(replied_m).mime_type}`"
+        text = f"**Dosya Adı:** `{_file_name}`\n\n" \
+               f"**Dosya uzantısı:** `{_file_name.rsplit('.', 1)[-1].upper()}`\n\n" \
+               f"**Dosya Türü:** `{get_file_type(replied_m).upper()}`\n\n" \
+               f"**Dosya Boyutu:** `{humanbytes(get_media_file_size(replied_m))}`\n\n" \
+               f"**Dosya MimeType:** `{get_file_attr(replied_m).mime_type}`"
         await cb.message.edit(
             text=text,
             parse_mode="Markdown",
